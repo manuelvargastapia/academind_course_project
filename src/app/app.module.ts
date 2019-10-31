571f76f1004940f8425ad3aa12fdf7b9a1ffc9e9
manuelvargastapia <manuelvargastapia@gmail.com>
2019-10-31 20:25:22 -0300

    Register new component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,10 @@ import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 
 import { AppComponent } from "./app.component";
+import { ServerComponent } from "./server/server.component";
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, ServerComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent]
