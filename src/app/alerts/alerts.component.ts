4ad3228108dc2b1a0c7ed5543454d486970308dd
manuelvargastapia <manuelvargastapia@gmail.com>
2019-10-31 21:30:55 -0300

    Assignment 1: add two new components, success and warning alerts

---
+++ src/app/alerts/alerts.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-alerts",
+  templateUrl: "alerts.component.html"
+})
+export class AlertsComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from "@angular/core";
 
 import { AppComponent } from "./app.component";
 import { ServerComponent } from "./server/server.component";
-import { ServersComponent } from './servers/servers.component';
+import { ServersComponent } from "./servers/servers.component";
+import { AlertsComponent } from "./alerts/alerts.component";
+import { WarningAlertComponent } from "./warning-alert/warning-alert.component";
+import { SuccessAlertComponent } from "./success-alert/success-alert.component";
 
 @NgModule({
-  declarations: [AppComponent, ServerComponent, ServersComponent],
+  declarations: [
+    AppComponent,
+    ServerComponent,
+    ServersComponent,
+    AlertsComponent,
+    WarningAlertComponent,
+    SuccessAlertComponent
+  ],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent]
